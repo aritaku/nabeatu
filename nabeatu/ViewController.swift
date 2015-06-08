1c169a5b65ddac6d6afca32fe76fbb854f578734
aritaku19@gmail.com <aritaku19@gmail.com>
2015-06-08 11:57:36 +0900

    leaders研修で使ったやつ

--- nabeatu/ViewController.swift
+++ nabeatu/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var number : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = String(number)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func isAho() -> Bool {
+        if number % 3 == 0 {
+            return true
+        } else {
+            return false
+        }
+        
+    }
 
+    @IBAction func plus(sender: AnyObject) {
+        
+        number += 1
+        
+        if isAho() == true {
+            label.text = "アホ\(number)"
+        } else {
+            label.text = String(number)
+        }
+    }
 
 }
 
